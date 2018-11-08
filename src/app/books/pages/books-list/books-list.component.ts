2735177368e035c44f8b6fcb5a86938e3263d6fa
dmitr <dmitriy321990@mail.ru>
2018-11-09 02:33:02 +0300

    Placed books from json to list component.

--- src/app/books/pages/books-list/books-list.component.ts
+++ src/app/books/pages/books-list/books-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {BooksService} from "../../../common/services/books.service";
+import {Book} from "../../../common/models/entities/book";
+import {take} from "rxjs/operators";
 
 @Component({
-  selector: 'bk-books-list',
-  templateUrl: './books-list.component.html',
-  styleUrls: ['./books-list.component.scss']
+    selector: "bk-books-list",
+    templateUrl: "./books-list.component.html",
+    styleUrls: ["./books-list.component.scss"]
 })
 export class BooksListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    
+    private books: Book[] = [];
+    private booksService: BooksService;
+    
+    constructor(booksService: BooksService) {
+        this.booksService = booksService;
+    }
+    
+    public ngOnInit() {
+        this.booksService.getBooks().pipe(
+            take(1)
+        )
+            .subscribe((books) => {
+                this.books = books;
+            });
+    }
+    
 }
