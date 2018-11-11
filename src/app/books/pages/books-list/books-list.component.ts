cf17d2bf17e423887aee63e9d6134d47a101fb77
dmitr <dmitriy321990@mail.ru>
2018-11-11 08:41:56 +0300

    Added logic to display books and animations to component.

--- src/app/books/pages/books-list/books-list.component.ts
+++ src/app/books/pages/books-list/books-list.component.ts
@@ -2,14 +2,20 @@ import {Component, OnInit} from "@angular/core";
 import {BooksService} from "../../../common/services/books.service";
 import {Book} from "../../../common/models/entities/book";
 import {take} from "rxjs/operators";
+import {bookListTrigger, BookState, bookTrigger} from "../../../common/animations/collapse.animations";
 
 @Component({
     selector: "bk-books-list",
     templateUrl: "./books-list.component.html",
-    styleUrls: ["./books-list.component.scss"]
+    styleUrls: ["./books-list.component.scss"],
+    animations: [
+        bookTrigger,
+        bookListTrigger
+    ]
 })
 export class BooksListComponent implements OnInit {
     
+    public selectedBook: Book | null = null;
     public books: Book[] = [];
     
     private booksService: BooksService;
@@ -27,4 +33,19 @@ export class BooksListComponent implements OnInit {
             });
     }
     
+    public onClickRow(event: Book): void {
+        this.selectedBook = event;
+    }
+    
+    public onHideBook(): void {
+        this.selectedBook = null;
+    }
+    
+    public getCardCollapsedState(): BookState {
+        return this.selectedBook ? BookState.Expanded : BookState.Collapsed;
+    }
+    
+    public getListCollpasedState(): BookState {
+        return this.selectedBook ? BookState.Collapsed : BookState.Expanded;
+    }
 }
