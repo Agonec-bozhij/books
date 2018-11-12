1ee6ae61ca619a49b8e34846e0353c40905200b4
dmitr <dmitriy321990@mail.ru>
2018-11-13 01:17:21 +0300

    Added functionality to upload image and preview.

--- src/app/books/pages/books-list/book/book-edit/book-edit.component.ts
+++ src/app/books/pages/books-list/book/book-edit/book-edit.component.ts
@@ -1,17 +1,23 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from "@angular/core";
 import {Book} from "../../../../../common/models/entities/book";
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {fromEvent, Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
     selector: "bk-book-edit",
     templateUrl: "./book-edit.component.html",
     styleUrls: ["./book-edit.component.scss"]
 })
-export class BookEditComponent implements OnInit {
+export class BookEditComponent implements OnInit, AfterViewInit, OnDestroy {
     
     @Input() public book!: Book;
     
     public form!: FormGroup;
+    
+    @ViewChild("imageInput") public imageInput: ElementRef<HTMLInputElement>;
+    
+    private destroy$ = new Subject<void>();
     
     constructor() {
         //
@@ -19,6 +25,15 @@ export class BookEditComponent implements OnInit {
     
     public ngOnInit(): void {
         this.initializeForm();
+    }
+    
+    public ngAfterViewInit(): void {
+        this.subscribeImageInput();
+    }
+    
+    public ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
     }
     
     private initializeForm(): void {
@@ -29,7 +44,8 @@ export class BookEditComponent implements OnInit {
             publisher: new FormControl(this.book.publisher, Validators.required),
             publicationYear: new FormControl(this.book.publicationYear, Validators.required),
             releaseDate: new FormControl(this.book.releaseDate, Validators.required),
-            isbn: new FormControl(this.book.isbn, Validators.required)
+            isbn: new FormControl(this.book.isbn, Validators.required),
+            image: new FormControl(this.book.image)
         });
     }
     
@@ -39,5 +55,21 @@ export class BookEditComponent implements OnInit {
                 name: new FormControl(author.name, Validators.required),
                 lastname: new FormControl(author.lastname, Validators.required)
             }));
+    }
+    
+    private subscribeImageInput(): void {
+        fromEvent(this.imageInput.nativeElement, "change").pipe(
+            takeUntil(this.destroy$)
+        ).subscribe((event: Event) => {
+            console.log("event", event);
+            
+            const reader = new FileReader();
+            const file: File = (event.target as HTMLInputElement).files[0];
+            reader.onloadend = () => {
+                this.form.get("image").setValue(reader.result);
+            };
+            
+            reader.readAsDataURL(file);
+        });
     }
 }
