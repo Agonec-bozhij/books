e2776d21d9ff2c2b83567a403780d1919887b291
dmitr <dmitriy321990@mail.ru>
2018-11-12 23:01:18 +0300

    Added book component to keep both view and edit states.

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -4,12 +4,14 @@ import {CommonModule} from "@angular/common";
 import {BooksRoutingModule} from "./books-routing.module";
 import {BooksListComponent} from "./pages/books-list/books-list.component";
 import {MaterialModule} from "../common/modules/material.module";
-import {BookViewComponent} from "./pages/books-list/book-view/book-view.component";
+import {BookViewComponent} from "./pages/books-list/book/book-view/book-view.component";
+import {BookComponent} from "./pages/books-list/book/book.component";
 
 @NgModule({
     declarations: [
         BooksListComponent,
-        BookViewComponent
+        BookViewComponent,
+        BookComponent
     ],
     imports: [
         CommonModule,
