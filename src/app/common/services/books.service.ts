80dd1bd56c45f47001088239a684e826cb41a535
dmitr <dmitriy321990@mail.ru>
2018-11-13 02:25:48 +0300

    Added CRUD methods in service.

--- src/app/common/services/books.service.ts
+++ src/app/common/services/books.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {BooksRepository} from "../repositories/books.repository";
-import {Observable} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 import {Book} from "../models/entities/book";
 
 @Injectable()
@@ -14,5 +14,17 @@ export class BooksService {
     
     public getBooks(): Observable<Book[]> {
         return this.booksRepository.getBooks();
+    }
+    
+    public createBook(book: Book): Observable<Book> {
+        return this.booksRepository.createBook(book);
+    }
+    
+    public updateBook(book: Book): Observable<Book> {
+        return this.booksRepository.updateBook(book);
+    }
+    
+    public deleteBook(book: Book): Observable<void> {
+        return this.booksRepository.deleteBook(book);
     }
 }
