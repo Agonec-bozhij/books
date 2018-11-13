172479b0012bc2e12c599f5627aa78432afb7afc
dmitr <dmitriy321990@mail.ru>
2018-11-14 00:57:33 +0300

    Improved book-edit component - connected it with service's book state and added create logic.

--- src/app/books/pages/books-list/book/book-edit/book-edit.component.ts
+++ src/app/books/pages/books-list/book/book-edit/book-edit.component.ts
@@ -1,10 +1,12 @@
-import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from "@angular/core";
+import {AfterViewInit, Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild} from "@angular/core";
 import {Book} from "../../../../../common/models/entities/book";
 import {AbstractControl, FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 import {fromEvent, Subject} from "rxjs";
 import {takeUntil} from "rxjs/operators";
 import {BooksService} from "../../../../../common/services/books.service";
 import {ToastrService} from "ngx-toastr";
+import {BookMode} from "../../../../../common/models/enums/book-mode";
+import {Author} from "../../../../../common/models/entities/author";
 
 @Component({
     selector: "bk-book-edit",
@@ -13,9 +15,16 @@ import {ToastrService} from "ngx-toastr";
 })
 export class BookEditComponent implements OnInit, AfterViewInit, OnDestroy {
     
-    @Input() public book!: Book;
-    
+    public BookMode = BookMode;
     public form!: FormGroup;
+    
+    public get book(): Book {
+        return this.booksService.getSelectedBook();
+    }
+    
+    public get mode(): BookMode {
+        return this.booksService.mode;
+    }
     
     @Output() public readonly cancel = new EventEmitter<void>();
     
@@ -66,28 +75,38 @@ export class BookEditComponent implements OnInit, AfterViewInit, OnDestroy {
     public onSubmit(): void {
         if (this.form.valid) {
             const book = new Book().fromJSON(this.form.value);
-            this.booksService.updateBook(book).subscribe(
-                () => this.toastrService.success(`Книга ${book.title} сохранена успешно`),
-                () => this.toastrService.success(`Произошла ошибка при сохранении книги ${book.title}`)
-            );
+            
+            if (this.booksService.mode === BookMode.Edit) {
+                this.booksService.updateBook(book).subscribe(
+                    () => this.toastrService.success(`Книга ${book.title} сохранена успешно`),
+                    () => this.toastrService.success(`Произошла ошибка при сохранении книги ${book.title}`)
+                );
+            } else {
+                this.booksService.createBook(book).subscribe(
+                    () => this.toastrService.success(`Книга ${book.title} создана успешно`),
+                    () => this.toastrService.success(`Произошла ошибка при создании книги ${book.title}`)
+                );
+            }
         }
     }
     
     private initializeForm(): void {
+        const edit: boolean | null = this.mode === BookMode.Edit || null;
+        
         this.form = new FormGroup({
-            title: new FormControl(this.book.title, Validators.required),
-            authors: new FormArray(this.getAuthorsGroups()),
-            pages: new FormControl(this.book.pages, Validators.required),
-            publisher: new FormControl(this.book.publisher, Validators.required),
-            publicationYear: new FormControl(this.book.publicationYear, Validators.required),
-            releaseDate: new FormControl(this.book.releaseDate, Validators.required),
-            isbn: new FormControl(this.book.isbn, Validators.required),
-            image: new FormControl(this.book.image)
+            title: new FormControl(edit && this.book.title, Validators.required),
+            authors: new FormArray(this.getAuthorsGroups(edit ? this.book.authors : [])),
+            pages: new FormControl(edit && this.book.pages, Validators.required),
+            publisher: new FormControl(edit && this.book.publisher, Validators.required),
+            publicationYear: new FormControl(edit && this.book.publicationYear, Validators.required),
+            releaseDate: new FormControl(edit && this.book.releaseDate, Validators.required),
+            isbn: new FormControl(edit && this.book.isbn, Validators.required),
+            image: new FormControl(edit && this.book.image)
         });
     }
     
-    private getAuthorsGroups(): FormGroup[] {
-        return this.book.authors
+    private getAuthorsGroups(authors: Author[]): FormGroup[] {
+        return authors
             .map((author) => new FormGroup({
                 name: new FormControl(author.name, Validators.required),
                 lastname: new FormControl(author.lastname, Validators.required)
